f0fce28c64c99367492083bde2c7e137af98f04c
hamzah <hamzah1faisal@gmail.com>
2024-10-28 11:27:58 +0000

    added .gitignore

--- nvim/lua/plugins/indent_blankline.lua
+++ nvim/lua/plugins/indent_blankline.lua
@@ -1,7 +1,34 @@
-return{
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+return {
+    { "HiPhish/rainbow-delimiters.nvim" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                highlight = {
+                    "TokyoRed",
+                    "TokyoRedder",
+                    "TokyoOrange",
+                    "TokyoGreen",
+                    "TokyoPurple",
+                    "TokyoCyan",
+                    "TokyoBlue",
+                },
+            },
+        },
+        config = function(_, opts)
+            local hooks = require("ibl.hooks")
+
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "TokyoRed", { fg = "#ff007c" })
+                vim.api.nvim_set_hl(0, "TokyoRedder", { fg = "#c53b53" })
+                vim.api.nvim_set_hl(0, "TokyoOrange", { fg = "#ff757f" })
+                vim.api.nvim_set_hl(0, "TokyoGreen", { fg = "#41a6b5" })
+                vim.api.nvim_set_hl(0, "TokyoPurple", { fg = "#9d7cd8" })
+                vim.api.nvim_set_hl(0, "TokyoCyan", { fg = "#7aa2f7" })
+                vim.api.nvim_set_hl(0, "TokyoBlue", { fg = "#3d59a1" })
+            end)
+
+            require("ibl").setup(opts)
+        end,
+    },
 }
